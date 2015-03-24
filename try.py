e0d0a916896d98da848289e25bc444c00b7e43f7
Ben Fowler <ben.greencanvas@gmail.com>
2015-03-24 21:52:19 +0000

    WIP

---
+++ try.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 smarttab expandtab:
+
+"""try.py: Tests the tent module"""
+
+__author__      = "Joe Salmon"
+__copyright__   = "Copyright 2015, Planet Earth"
+
+import tent
+import sqlite3, csv
+from sys import argv 
+
+filename = 'work/councilfull.csv'
+
+# This tells python to start using SQLITE3 with a database called whatever what typed after python joesqlite.py.
+conn = sqlite3.connect( 'db' )
+
+# This tells python to start typing in the running sqlite3 program.
+cur = conn.cursor()
+
+tent.importcsvfile( filename )
