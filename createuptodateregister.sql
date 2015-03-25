8d53e7b9311519c778d5cbc3a0d23b659147ddfa
Joesalmon1985 <Joesalmon1985@gmail.com>
2015-03-25 13:12:46 +0000

    Update createuptodateregister.sql

--- createuptodateregister.sql
+++ createuptodateregister.sql
@@ -40,6 +40,24 @@ DROP TABLE if exists ward28;
 .import P141201_Garforth_and_Swillington.csv ward10
 .import P141201_Gipton_and_Harehills.csv ward11
 .import P141201_Guiseley_and_Rawdon.csv ward12
+.import P141201_Harewood.csv ward13
+.import P141201_Headingley.csv ward14
+.import P141201_Horsforth.csv ward15
+.import P141201_Hyde_Pary_and_Woodhouse.csv ward16
+.import P141201_Killingbeck_and_Secroft.csv ward17
+.import P141201_Kippax_and_Methley.csv ward18
+.import P141201_Middleton_Park.csv ward19
+.import P141201_Moortown.csv ward20
+.import P141201_Morley_North.csv ward21
+.import P141201_Morley_South.csv ward22
+.import P141201_Otley_and_Yeadon.csv ward23
+.import P141201_Pudsey.csv ward24
+.import P141201_Rothwell.csv ward25
+.import P141201_Roundhay.csv ward26
+.import P141201_Temple_Newsam.csv ward27
+.import P141201_Weetwood.csv ward28
+.import P141201_Wetherby.csv ward29
+
 
 drop table if exists alldata;
 CREATE TABLE alldata(
@@ -69,6 +87,32 @@ insert into alldata select * from ward01;
 insert into alldata select * from ward02;
 insert into alldata select * from ward03;
 insert into alldata select * from ward04;
+insert into alldata select * from ward05;
+insert into alldata select * from ward06;
+insert into alldata select * from ward07;
+insert into alldata select * from ward08;
+insert into alldata select * from ward09;
+insert into alldata select * from ward10;
+insert into alldata select * from ward11;
+insert into alldata select * from ward12;
+insert into alldata select * from ward13;
+insert into alldata select * from ward14;
+insert into alldata select * from ward15;
+insert into alldata select * from ward16;
+insert into alldata select * from ward17;
+insert into alldata select * from ward18;
+insert into alldata select * from ward19;
+insert into alldata select * from ward20;
+insert into alldata select * from ward21;
+insert into alldata select * from ward22;
+insert into alldata select * from ward23;
+insert into alldata select * from ward24;
+insert into alldata select * from ward25;
+insert into alldata select * from ward26;
+insert into alldata select * from ward27;
+insert into alldata select * from ward28;
+insert into alldata select * from ward29;
+
 
 drop table if exists fulldata;
 CREATE TABLE fulldata(
@@ -178,7 +222,6 @@ insert into uptodatecouncildata (pd, eno, stat, title, firstname, initials, surn
 delete from uptodatecouncildata where rowid not in 
 (select min(rowid) from uptodatecouncildata group by firstname, surname, address1);
 
-.headers on
-.output uptodatecouncildata.csv
-select * from uptodatecouncildata;
+
+select * from membercouncildata;
 .output stdout
